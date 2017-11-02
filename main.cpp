04ea492f715f4a33d310297d18799c2ade67a7a8
Anthony Gendron <aperturescience540@gmail.com>
2017-11-02 13:15:22 -0400

    Lab_7_1_1

--- main.cpp
+++ main.cpp
@@ -11,15 +11,24 @@
  * Created on November 2, 2017, 12:47 PM
  */
 
-#include <cstdlib>
+#include <iostream> 
 
-using namespace std;
+using namespace std; 
 
-/*
- * 
- */
-int main(int argc, char** argv) {
-
+int main(void) {
+    
+    int a = 8, b = 0, c = 0;
+    cin >> b;
+    try {
+        if(b == 0)
+            throw string("Your input is not valid, you can't divide by zero.");
+        c = a / b;
+        cout << c << endl;
+    }
+    catch(string &problem) {
+        cout << problem << endl;
+        cout << c << endl;
+    }
     return 0;
 }
 
